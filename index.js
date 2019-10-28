b4b4005dc2a7203c5f404e88184c7ca8bce20f27
unknown <nicholas.gebhart@gmail.com>
2019-10-28 12:34:23 -0700

    feat: add find by id

--- index.js
+++ index.js
@@ -17,5 +17,31 @@ server.post("/api/users", (req, res) => {
     });
 });
 
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.status(201).json(users))
+    .catch(err =>
+      res.status(500).json({ response: "Unable to fetch users", err: err })
+    );
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      }
+      res.status(404).json({ response: "user does not exist" });
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ response: "you have made a mistake my dude", err: err })
+    );
+});
+
 const port = 8000;
 server.listen(port, () => console.log("port is running on 8000"));
