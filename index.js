2b57a4d7bcd48c0f04a986687c5ad473b228df6b
unknown <nicholas.gebhart@gmail.com>
2019-10-28 12:23:00 -0700

    feat: add post endpoint

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const db = require("./data/db.js");
 
 server.use(express.json());
 server.use(cors());
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  db.insert(user)
+    .then(user => res.status(201).json(user))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ response: "you have goofed" });
+    });
+});
+
+const port = 8000;
+server.listen(port, () => console.log("port is running on 8000"));
